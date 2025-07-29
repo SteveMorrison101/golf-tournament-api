4f633bd96daa00493b3be04161255bd26b7d1bd6
Steve Morrison <smorrison@mun.ca>
2025-07-29 17:33:08 -0230

    Added MemberService and TournamentService

---
+++ src/main/java/com/golfclub/service/MemberService.java
@@ -0,0 +1,38 @@
+package com.golfclub.service;
+
+import com.golfclub.model.Member;
+import com.golfclub.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MemberService {
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public MemberService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public List<Member> getAllMembers() {
+        return memberRepository.findAll();
+    }
+
+    public Optional<Member> getMemberById(Long id) {
+        return memberRepository.findById(id);
+    }
+
+    public Member addMember(Member member) {
+        return memberRepository.save(member);
+    }
+
+    public void deleteMember(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    // Will dd more methods for searching later
+}
+
---
+++ src/main/java/com/golfclub/service/TournamentService.java
@@ -0,0 +1,38 @@
+package com.golfclub.service;
+
+import com.golfclub.model.Tournament;
+import com.golfclub.repository.TournamentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class TournamentService {
+    private final TournamentRepository tournamentRepository;
+
+    @Autowired
+    public TournamentService(TournamentRepository tournamentRepository) {
+        this.tournamentRepository = tournamentRepository;
+    }
+
+    public List<Tournament> getAllTournaments() {
+        return tournamentRepository.findAll();
+    }
+
+    public Optional<Tournament> getTournamentById(Long id) {
+        return tournamentRepository.findById(id);
+    }
+
+    public Tournament addTournament(Tournament tournament) {
+        return tournamentRepository.save(tournament);
+    }
+
+    public void deleteTournament(Long id) {
+        tournamentRepository.deleteById(id);
+    }
+
+    // Will add methods for searching and member registration later
+}
+
