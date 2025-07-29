519f2a6f068af9a7247f34b6ad69fdbfde23a50d
Steve Morrison <smorrison@mun.ca>
2025-07-29 17:24:19 -0230

    Added MemberRepository and TournamentRepository interfaces

---
+++ src/main/java/com/golfclub/repository/MemberRepository.java
@@ -0,0 +1,10 @@
+package com.golfclub.repository;
+
+import com.golfclub.model.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MemberRepository extends JpaRepository<Member, Long> {
+}
+
